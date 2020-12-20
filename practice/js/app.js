3e6227ad432bf2b769df018401cecfefacb2c590
World-Mozg <73451241+World-Mozg@users.noreply.github.com>
2020-12-20 21:40:41 +0600

    Add files via upload

---
+++ practice/js/app.js
@@ -0,0 +1,7 @@
+/* Burger */
+let nav = $("#nav");
+
+$("#navToggle").on("click", function(event) {
+    event.preventDefault(); /* действие по умолчанию не выполнится */
+    nav.toggleClass("show"); /* добавить класс */
+})
